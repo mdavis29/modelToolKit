af7df886e240bc288682219eba0475dfe660e2de
Davis <mad221@musc.edu>
2017-03-23 10:29:07 -0400

    getZip now works with PCA

---
+++ R/getZip.R
@@ -0,0 +1,36 @@
+#' @title get Zip
+#' @param zipCodes a vector of vip codes
+#' @param pca logical, whether or not to return principal components
+#' @description looks up zip codes 2014 IRS tax data aggregated
+#' @details returns a dataframe with demographics for each zipcode,
+#' If the zipcode is not found, it returns the mean of all zipcodes
+#' @author Matthew Davis
+#' @export
+getZip<-function(zipCodes, pca = FALSE){
+  zc<-as.numeric(zipCodes)
+  demCols<-colnames(irs)[!colnames(irs) %in%  c('zipCode', 'stateCode')]
+  irs.temp<-irs[irs$zipCode %in% zc,]
+  avg<-sapply(irs[,demCols],
+              mean,
+              na.rm = TRUE)
+  validTest<-!zc %in% c(0,00000,NA,99999) &
+                nchar(as.character(zc)) == 5
+  output<-NULL
+  output.temp<-NULL
+  for ( i in 1:(length(zc))){
+    if(validTest[i] == TRUE){
+    output.temp<-irs.temp[irs.temp$zipCode == zc[i],demCols][1,]
+      }
+    if(validTest[i] == FALSE | is.null(output.temp) ){
+    output.temp<-avg
+      }
+    output<-rbind(output, output.temp)
+    output.temp<-NULL
+    }
+  if(pca == TRUE){
+    pc<-predict(preProc, output)
+    colnames(pc)<-paste('zipCode', colnames(pc), sep = '')
+    output<-cbind(output, pc)
+    }
+  return(output)
+}
