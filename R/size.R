edec3101a8c3aee56c154ca4c7014215fd7610d7
Davis <mad221@musc.edu>
2017-04-06 11:30:33 -0400

    added size function

---
+++ R/size.R
@@ -0,0 +1,11 @@
+#' @title size
+#' @param mydata an r object
+#' @param units
+#' @return a data frame of preformance metrics for all the models in the enviroments
+#' @description usefull if you save a bunch models in an enviroment and want to test them all
+#' @author Matthew Davis
+#' @details this function is for choosing the best model based on a test set
+#' @export
+size<-function(mydata,units = 'Mb'){
+  format(object.size(mydata), units =units)
+}
