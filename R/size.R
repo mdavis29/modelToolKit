9dda30341f4ffb694b0aa7b7cdb7c80025950bfd
Davis <mad221@musc.edu>
2017-04-07 08:20:32 -0400

    changed size to size2 and fixed env test

--- R/size.R
+++ R/size.R
@@ -1,4 +1,4 @@
-#' @title size
+#' @title sizeit
 #' @param mydata an r object
 #' @param units units in the format(object.size) method
 #' @return a data frame of preformance metrics for all the models in the enviroments
@@ -6,6 +6,6 @@
 #' @author Matthew Davis
 #' @details this function is for choosing the best model based on a test set
 #' @export
-size<-function(mydata,units = 'Mb'){
+sizeit<-function(mydata,units = 'Mb'){
   format(object.size(mydata), units =units)
 }
